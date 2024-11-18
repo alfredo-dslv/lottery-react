65c94070512d11a2a4636c2b590a299966b80582
Alfredo <alfredo.dslv@gmail.com>
2024-11-18 16:55:55 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import './App.css';
 import web3 from './web3';
 
 function App() {
-  console.log(web3.version);
-  web3.eth.getAccounts()
-  .then(console.log);
-  
-  return (
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
